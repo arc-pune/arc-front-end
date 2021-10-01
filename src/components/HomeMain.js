48d318edcca99f4c589659ab36fc3d61da0c3323
AnandDhakane01 <ananddhakane01@gmail.com>
2021-10-01 12:41:35 +0530

    updated homepage

--- src/components/HomeMain.js
+++ src/components/HomeMain.js
@@ -7,13 +7,20 @@ const HomeMain = () => {
         <div className="home-main lg:flex items-center sm:justify-center">
           <div className="left lg:w-1/2">
             <h2 className="text-4xl text-center">
-              Our team will teach you the art of music and melody.
+              Raising Hands For Injured Paws
             </h2>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
+              Animal Rescue Community (ARC) Pune is a group of college students
+              raising hands for injured paws since 2020. We are a non-profit
+              community organization of around 60 active volunteers, and many
+              passive volunteers all over pune. Feeding, Rescue and Adoptions
+              are our main domain of work. Our volunteers daily feed 500+ dogs
+              and cats in their respective areas. Adoption is our prominent
+              feature. We have been a part of 225+ successful adoptions till
+              date. We also take rescue cases enthusiastically. We believe in
+              coexistence and work for reducing human-animal conflicts. ARC has
+              recently started working in nanded, being the first ever animal
+              welfare organization in the city.
             </p>
             <div class="p-2 w-full">
               <button class="flex mx-auto text-white bg-black border-0 py-2 px-8 focus:outline-none rounded-xl text-lg">
