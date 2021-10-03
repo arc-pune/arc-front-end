bf42b54453d011ebf5b157e2dbb0b09ccdfe25fe
rohancodex <rohancodex@gmail.com>
2021-10-03 23:56:45 +0530

    Created Footer component
    Footer component created according to the current UI design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,16 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import HomeMain from "../components/HomeMain";
+import { Footer } from './../components/Footer';
 
 const Home = () => {
   return (
     <div>
-       //nav bar 
       <NavBar></NavBar>
       <HomeMain/>
-    
+      <Footer />
     </div>
+ 
   );
 };
 
