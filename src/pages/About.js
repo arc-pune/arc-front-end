c32e0f1331b6aea4a4838352103ff364eb31d264
Vishnu Kulkarni <blackdragon4204@gmail.com>
2021-10-04 00:01:02 +0530

    Created About page
    Created about Page and About Main Component

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+
+import AboutMain from "../components/AboutMain";
+const About = () => {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <AboutMain />
+    </div>
+  );
+};
+
+export default About;
