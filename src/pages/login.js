075962df6cf930d8b0d406e5826f748aea269dae
Anand Dhakane <ananddhakane01@gmail.com>
2021-10-05 14:54:06 +0530

    Revert "Updated Login Page"

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,18 +2,9 @@ import React from "react";
 import "./login.css";
 import NavBar from "../components/NavBar";
 
-const Login = ({setLogin}) => {
-  console.log("Am inside samplelogin")
+const Login = () => {
   return (
-    <div class="min-w-screen h-screen animated fadeIn faster  fixed  left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center bg-cover">
-     <div class="absolute bg-black opacity-80 inset-0 z-0"></div>
-    <div class="w-full  max-w-7xl p-5 relative mx-auto my-auto rounded-xl shadow-lg  ">
-      
-      <div class="">
-       <div className="cancelparent">
-       <button className="cancel" onClick={() => {setLogin(false)}}>&times;</button>
-       </div>
-       <div class="login">
+    <div class="login">
       <div class="section-1">
         <div class="content">
           <div class="image">
@@ -56,12 +47,6 @@ const Login = ({setLogin}) => {
         </form>
       </div>
     </div>
-
-        
-      </div>
-    </div>
-  </div>
-
   );
 };
 
