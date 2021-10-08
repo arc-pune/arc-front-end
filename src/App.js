7d484918d44c26e7330bed308afabc8c53c0b16e
AnandDhakane01 <ananddhakane01@gmail.com>
2021-10-08 19:32:51 +0530

    updated readme, changed text on the homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import About from "./pages/About";
 import PetDetailMain from "./components/PetDetailMain";
 
--- src/utilities/themeChange.js
+++ src/utilities/themeChange.js
@@ -1,9 +1,9 @@
 export function initTheme() {
-  const themes = ["dark", "light", "neon"];
-  localStorage.themes = JSON.stringify(themes);
-  localStorage.theme = "light";
+    const themes = ["dark", "light", "neon"];
+    localStorage.themes = JSON.stringify(themes);
+    localStorage.theme = "light";
 }
 
 export function setTheme(theme) {
-  localStorage.theme = theme;
+    localStorage.theme = theme;
 }
