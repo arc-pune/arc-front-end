66df5262c31866f40b3d3abfb76fb7c4ba06b535
majjikishore007 <majjikishore031@gmail.com>
2021-10-08 21:45:56 +0530

    removed log errors

--- src/components/AboutMain.js
+++ src/components/AboutMain.js
@@ -5,13 +5,13 @@ import "./aboutMain.css";
 const AboutMain = () => {
   return (
     <>
-      <div className="aboutmain__background"></div>
-      <section className="">
+      <div className='aboutmain__background'></div>
+      <section className=''>
         {/*our organization section */}
-        <div className="aboutmain___ourOrganization">
-          <img src="/Assets/icons/paws.svg" alt="" />
+        <div className='aboutmain___ourOrganization'>
+          <img src='/Assets/icons/paws.svg' alt='' />
           <h2>Our organization</h2>
-          <div className="aboutmain___ourOrganizationMain">
+          <div className='aboutmain___ourOrganizationMain'>
             <p>
               {" "}
               Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -20,8 +20,8 @@ const AboutMain = () => {
               type and scrambled it to make a type specimen book.
             </p>
           </div>
-          <div className="end">
-            <img src="/Assets/icons/paws.svg" alt="" />
+          <div className='end'>
+            <img src='/Assets/icons/paws.svg' alt='' />
           </div>
         </div>
         {/*Our Founder section */}
@@ -30,47 +30,47 @@ const AboutMain = () => {
 
         {/**Our Main Contributor Section */}
 
-        <div className="ourMainContributor__header my-24">
-          <img src="/Assets/icons/paws.svg" alt="" />
-          <h2 className="">Main Contributors</h2>
+        <div className='ourMainContributor__header my-24'>
+          <img src='/Assets/icons/paws.svg' alt='' />
+          <h2 className=''>Main Contributors</h2>
         </div>
 
-        <div className="grid grid-cols-none  justify-center align-center my-24  mx-auto  lg:grid-cols-3 mt-0.5   ">
+        <div className='grid grid-cols-none  justify-center align-center my-24  mx-auto  lg:grid-cols-3 mt-0.5   '>
           <MainContributor
-            name=" Minal Rai"
-            role="Ui/Ux Designer"
-            Github=""
-            linkedin=""
-            Twitter=""
-            image="https://images.pexels.com/photos/3270230/pexels-photo-3270230.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            name=' Minal Rai'
+            role='Ui/Ux Designer'
+            Github=''
+            linkedin=''
+            Twitter=''
+            image='https://images.pexels.com/photos/3270230/pexels-photo-3270230.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
           />
           <MainContributor
-            name=" Ayush Kejriwal"
-            role="Ui/Ux Designer"
-            image="https://images.pexels.com/photos/594610/pexels-photo-594610.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            name=' Ayush Kejriwal'
+            role='Ui/Ux Designer'
+            image='https://images.pexels.com/photos/594610/pexels-photo-594610.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
           />
           <MainContributor
-            name=" Minal Rai"
-            role="Ui/Ux Designer"
-            image="https://images.unsplash.com/photo-1534751516642-a1af1ef26a56?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=389&q=80"
+            name=' Minal Rai'
+            role='Ui/Ux Designer'
+            image='https://images.unsplash.com/photo-1534751516642-a1af1ef26a56?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=389&q=80'
           />{" "}
           <MainContributor
-            name=" Ayush Kejriwal"
-            role="Ui/Ux Designer"
-            image="https://images.pexels.com/photos/594610/pexels-photo-594610.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            name=' Ayush Kejriwal'
+            role='Ui/Ux Designer'
+            image='https://images.pexels.com/photos/594610/pexels-photo-594610.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
           />{" "}
           <MainContributor
-            name=" Minal Rai"
-            role="Ui/Ux Designer"
-            Github=""
-            linkedin=""
-            Twitter=""
-            image="https://images.unsplash.com/photo-1534751516642-a1af1ef26a56?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=389&q=80"
+            name=' Minal Rai'
+            role='Ui/Ux Designer'
+            Github=''
+            linkedin=''
+            Twitter=''
+            image='https://images.unsplash.com/photo-1534751516642-a1af1ef26a56?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=389&q=80'
           />
           <MainContributor
-            name=" Ayush Kejriwal"
-            role="Ui/Ux Designer"
-            image="https://images.pexels.com/photos/594610/pexels-photo-594610.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            name=' Ayush Kejriwal'
+            role='Ui/Ux Designer'
+            image='https://images.pexels.com/photos/594610/pexels-photo-594610.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
           />
         </div>
       </section>
