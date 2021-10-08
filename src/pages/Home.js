fed43430cf7b5a25032a4166562fbee9321c7f48
rohancodex <rohancodex@gmail.com>
2021-10-08 16:45:21 +0530

    Created slider and founders section
    Created slider and founders section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,11 @@ import NavBar from "../components/NavBar";
 import HomeMain from "../components/HomeMain";
 import { Footer } from "./../components/Footer";
 import Gallery from "../components/Gallery";
-import TestimonialCard from "../components/TestimonialCard";
+import { TestimonialsMain } from "../components/TestimonialsMain";
+import FounderWord from "../components/FounderWord";
+
+
+
 
 const Home = () => {
   return (
@@ -11,23 +15,8 @@ const Home = () => {
       <NavBar />
       <HomeMain />
       <Gallery />
-      <div className="flex flex-col md:flex-row">
-        <TestimonialCard
-          image={
-            "https://images.unsplash.com/photo-1603570112520-fdc514048979?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=736&q=80"
-          }
-        />
-        <TestimonialCard
-          image={
-            "https://images.unsplash.com/photo-1603570112520-fdc514048979?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=736&q=80"
-          }
-        />
-        <TestimonialCard
-          image={
-            "https://images.unsplash.com/photo-1603570112520-fdc514048979?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=736&q=80"
-          }
-        />
-      </div>
+      <TestimonialsMain/>
+      <FounderWord/>
       <Footer />
     </div>
   );
