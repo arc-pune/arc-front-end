cd402b2767a2f7c25e193f59adc02dad93c00ba0
gayathri-ms <b219024@iiit-bh.ac.in.com>
2021-10-09 23:20:15 +0530

    updating login page

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,94 +1,145 @@
 import React from "react";
+import { useRef, useCallback } from "react";
+import {
+  motion,
+  useViewportScroll,
+  useTransform,
+  useAnimation,
+} from "framer-motion";
 
 const Gallery = () => {
+  const controls = useAnimation();
+  const observer = useRef();
+  const pawsElementRef = useCallback((node) => {
+    if (observer.current) observer.current.disconnect();
+    observer.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        controls.start("visible");
+        console.log("on screen");
+      } else {
+        controls.start("hidden");
+      }
+    });
+    if (node) observer.current.observe(node);
+  });
+  
   return (
-    <div className='text-gray-400 h-1/2 body-font mt-32'>
-      <div className='container px-5 pb-2 mx-auto justify-center flex flex-wrap'>
-        <div className='flex w-full mb-5 justify-center flex-wrap'>
-          <h1 className='sm:text-3xl text-2xl md:font-sans font-bold title-font text-black lg:w-1/3 lg:mb-0 mb-2 dark:text-white'>
-            Some of the Adopted ones
+
+    
+    <div className="text-gray-400 h-1/2 body-font ">
+      <div className="container px-5 py-24 mx-auto justify-center flex flex-wrap">
+      {/* <div ref={pawsElementRef} className='h-24 md:h-52'>
+        <motion.img
+          variants={{
+            hidden: {
+              x: -100,
+            },
+            visible: {
+              x: 0,
+              transition: {
+                delay: 0.3,
+                ease: "easeOut",
+              },
+            },
+          }}
+          initial='hidden'
+          animate={controls}
+          src='/Assets/images/paws.png'
+          alt='paws'
+          className='h-full'
+        />
+      </div> */}
+      <img
+          alt="gallery"
+          class="w-1/10 object-cover h-full -mt-24 -ml-20 object-center block"
+          src="/Assets/images/dogbaba.gif"
+        />
+        <div className="flex w-full mb-5 justify-center flex-wrap">
+          <h1 className="sm:text-3xl text-2xl md:font-sans font-bold title-font text-white lg:w-1/3 lg:mb-0 mb-2">
+            Some of the Adopted one's
           </h1>
         </div>
-        <div className='flex w-full mb-10 justify-center flex-wrap'>
-          <p className='lg:pl-6 lg:w-2/3 md:font-sans font-medium leading-relaxed text-dark-grey'>
+        <div className="flex w-full mb-10 justify-center flex-wrap">
+          <p className="lg:pl-6 lg:w-2/3 md:font-sans font-medium leading-relaxed text-dark-grey">
             Feeding, Rescue and Adoptions are our main domain of work. Our
             volunteers daily feed 500+ dogs and cats in their respective areas.
             Adoption is our prominent feature.
           </p>
         </div>
-        <div className='flex flex-wrap md:max-w-7xl'>
-          <div className='flex flex-wrap justify-end items-center w-1/3'>
-            <div className='md:p-2 px-1 py-1 w-1/2'>
+        <div className="flex flex-wrap md:max-w-7xl">
+          <div class="flex flex-wrap justify-end items-center w-1/2">
+            <div class="md:p-2 px-1 py-1 w-1/2">
               <img
-                alt='gallery'
-                className='w-full object-cover h-full object-center block rounded-tl-xl lg:rounded-tl-3xl'
-                src='https://source.unsplash.com/1600x900/?dog&id=98'
+                alt="gallery"
+                class="w-full object-cover h-full object-center block rounded-tl-3xl"
+                src="https://source.unsplash.com/1600x900/?dog&id=98"
               />
             </div>
-            <div className='md:p-2 px-1 py-1 w-1/2'>
+            <div class="md:p-2 px-1 py-1 w-1/2">
               <img
-                alt='gallery'
-                className='w-full object-cover h-full object-center block'
-                src='https://source.unsplash.com/1600x900/?dog&id=41'
+                alt="gallery"
+                class="w-full object-cover h-full object-center block"
+                src="https://source.unsplash.com/1600x900/?dog&id=41"
               />
             </div>
-            <div className='md:p-2 px-1 py-1 w-full'>
+            <div class="md:p-2 px-1 py-1 w-full">
               <img
-                alt='gallery'
-                className='w-full h-full object-cover object-center block rounded-bl-xl lg:rounded-bl-3xl'
-                src='https://source.unsplash.com/1600x900/?dog&id=33'
-              />
-            </div>
-          </div>
-          <div className='flex flex-wrap justify-center items-center w-1/3'>
-            <div className='md:p-2 px-1 py-1 w-full'>
-              <img
-                alt='gallery'
-                className='w-full h-full object-cover object-center block'
-                src='https://source.unsplash.com/1600x900/?dog&id=19'
-              />
-            </div>
-            <div className='md:p-2 px-1 py-1 w-1/2'>
-              <img
-                alt='gallery'
-                className='w-full object-cover h-full object-center block'
-                src='https://source.unsplash.com/1600x900/?cat&id=14'
-              />
-            </div>
-            <div className='md:p-2 px-1 py-1 w-1/2'>
-              <img
-                alt='gallery'
-                className='w-full object-cover h-full object-center block'
-                src='https://source.unsplash.com/1600x900/?dog&id=61'
+                alt="gallery"
+                class="w-full h-full object-cover object-center block rounded-bl-3xl"
+                src="https://source.unsplash.com/1600x900/?dog&id=33"
               />
             </div>
           </div>
-          <div className='flex flex-wrap justify-start items-center w-1/3'>
-            <div className='md:p-2 px-1 py-1 w-1/2'>
+          <div class="flex flex-wrap justify-center items-center w-1/2">
+            <div class="md:p-2 px-1 py-1 w-full">
               <img
-                alt='gallery'
-                className='w-full object-cover h-full object-center block'
-                src='https://source.unsplash.com/1600x900/?cat&id=17'
+                alt="gallery"
+                class="w-full h-full object-cover object-center block rounded-tr-3xl"
+                src="https://source.unsplash.com/1600x900/?dog&id=19"
               />
             </div>
-            <div className='md:p-2 px-1 py-1 w-1/2'>
+            <div class="md:p-2 px-1 py-1 w-1/2">
               <img
-                alt='gallery'
-                className='w-full object-cover h-full object-center block rounded-tr-xl lg:rounded-tr-3xl'
-                src='https://source.unsplash.com/1600x900/?dog&id=88'
+                alt="gallery"
+                class="w-full object-cover h-full object-center block"
+                src="https://source.unsplash.com/1600x900/?cat&id=14"
               />
             </div>
-            <div className='md:p-2 px-1 py-1 w-full'>
+            <div class="md:p-2 px-1 py-1 w-1/2">
               <img
-                alt='gallery'
-                className='w-full h-full object-cover object-center block rounded-br-xl lg:rounded-br-3xl'
-                src='https://source.unsplash.com/1600x900/?rabbit&id=99'
+                alt="gallery"
+                class="w-full object-cover h-full object-center block rounded-br-3xl"
+                src="https://source.unsplash.com/1600x900/?dog&id=61"
               />
             </div>
           </div>
+          {/* <div class="flex flex-wrap justify-start items-center w-1/3">
+            <div class="md:p-2 px-1 py-1 w-1/2">
+              <img
+                alt="gallery"
+                class="w-full object-cover h-full object-center block"
+                src="https://source.unsplash.com/1600x900/?cat&id=17"
+              />
+            </div>
+            <div class="md:p-2 px-1 py-1 w-1/2">
+              <img
+                alt="gallery"
+                class="w-full object-cover h-full object-center block rounded-tr-xl lg:rounded-tr-3xl"
+                src="https://source.unsplash.com/1600x900/?dog&id=88"
+              />
+            </div>
+            <div class="md:p-2 px-1 py-1 w-full">
+              <img
+                alt="gallery"
+                class="w-full h-full object-cover object-center block rounded-br-xl lg:rounded-br-3xl"
+                src="https://source.unsplash.com/1600x900/?rabbit&id=99"
+              />
+            </div>
+          </div> */}
         </div>
       </div>
     </div>
+    
   );
 };
 
